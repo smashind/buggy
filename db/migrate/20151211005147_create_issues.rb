a9c4c44d5ca10fc7fd19ac51f403e638691b79a7
Danny Baumann <danthemanvt@gmail.com>
2015-12-10 19:18:39 -0800

    Display project issues

---
+++ db/migrate/20151211005147_create_issues.rb
@@ -0,0 +1,11 @@
+class CreateIssues < ActiveRecord::Migration
+  def change
+    create_table :issues do |t|
+      t.string :name
+      t.text :description
+      t.references :project, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202113115) do
+ActiveRecord::Schema.define(version: 20151211005147) do
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "issues", ["project_id"], name: "index_issues_on_project_id"
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
